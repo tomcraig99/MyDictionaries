36e02e49cdbb1e7eb2aeeaa56b9ef1df1a85d53f
Thomas Craig <tomcraig99@gmail.com>
2022-02-14 14:46:13 -0600

    updated in class

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,24 +1,24 @@
-#open file
-infile = open('AI.txt', 'r')
-#right sentence to list and split it up by spaces
+# open file
+infile = open("AI.txt", "r")
+# right sentence to list and split it up by spaces
 sentence = infile.readline()
 tempList = sentence.split()
 sentenceList = []
-#loop to write alphanumeric char to temp word and add to list
+# loop to write alphanumeric char to temp word and add to list
 for word in tempList:
-    temp = ''
+    temp = ""
     if not word.isalnum():
         for char in word:
             if char.isalnum():
-                temp+=char              
-        word=temp
-    sentenceList.append(word)
-#create dictionary
+                temp += char
+        word = temp
+    sentenceList.append(word.lower())
+# create dictionary
 wordCount = {}
-#add keys to dictionary
+# add keys to dictionary
 for key in sentenceList:
     value = sentenceList.count(key)
-    wordCount[key.lower()] = value
-#display keys and their values
+    wordCount[key] = value
+# display keys and their values
 for key in wordCount:
     print(key, wordCount[key])
